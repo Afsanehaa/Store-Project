4db2b706e35e6b4bd32c5c822a36c72dfbcc4fae
unknown <afsanehabdid@gmail.com>
2023-05-28 14:04:56 +0330

    make counter for landing numbers

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 const $=document
 const landingTitle =$.querySelector('.landing__title')
+const landingCoursesCount=$.querySelector('#courses-count')
+const landingMiutesCounte=$.querySelector('#miutes-counter')
+const landingUsersCounte=$.querySelector('#user-counter')
+
+
 
 window.addEventListener('load',()=> {
     let landingText = "ما به هر قیمتی دوره آموزشی تولید نمیکنیم !";
-    let typeIndex =0;
-    typewriter(landingText ,typeIndex)
+    let typeIndex = 0;
+    typewriter(landingText ,typeIndex);
+    makeCounter(40 , landingCoursesCount);
+    makeCounter(1098 , landingMiutesCounte);
+    makeCounter(310 , landingUsersCounte);
 
 });
 
@@ -15,5 +23,16 @@ function typewriter(text , index){
     }
     setTimeout(()=>{
         typewriter(text , index);
-    },100)
-}
+    },100);
+};
+function makeCounter(max ,elem){
+    let counter = 0;
+    const interval = setInterval(()=>{
+        if(counter === max){
+            clearInterval(interval);
+        }
+        elem.innerHTML = counter;
+        counter++;
+        
+    },0.5);
+};
